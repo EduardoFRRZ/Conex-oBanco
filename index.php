1512b894d7cc87f5832f97397e38ba1046bf7e05
Eduardo Ferrarezi <eduardogd20@gmail.com>
2018-08-27 21:42:55 -0300

    colunas bootStrap

--- index.php
+++ index.php
@@ -8,60 +8,79 @@
 		<meta charset="utf-8">
 		<title>Conecxao com Banco</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	</head>
-	<body>
+	<body background="img/fundo.jpg">
 
-		<div class="form">
-			<form method="POST" action="inserir.php">
-				
-				</br></br>
+		<h1 align="center">CONEXÃO COM O MYSQL</h1>
 
-				<label>Id</label>
-				</br>
-				<input type="text" name="id">
+		<div class="container">	
 
-				</br></br>
+			<div class="row">
+				<div class="col">
 
-				<label>Nome</label>
-				</br>
-				<input type="text" name="nome">
+					<div class="form">
+						<form method="POST" action="inserir.php">
+								
+							</br></br>
 
-				</br></br>
+							<label>Id</label>
+							</br>
+							<input class="form-control" type="text" name="id">
 
-				<label>Idade</label>
-				</br>
-				<input type="text" name="idade">
+							</br></br>
 
-				</br></br>
+							<label>Nome</label>
+							</br>
+							<input class="form-control" type="text" name="nome">
 
-				<label>Altura</label>
-				</br>
-				<input type="text" name="altura">
+							</br></br>
 
-				</br></br>
+							<label>Idade</label>
+							</br>
+							<input class="form-control" type="text" name="idade">
 
-				<input type="submit" value="Enviar">
+							</br></br>
 
-			</form>
+							<label>Altura</label>
+							</br>
+							<input class="form-control" type="text" name="altura">
+
+							</br></br>
+
+							<input class="btn btn-primary btn-lg" type="submit" value="Enviar">
+							<input class="btn btn-danger btn-lg" type="submit" value="Limpar tabela">
+
+						</form>
+					</div>
+				</div>
+
+				<div class="col">
+
+					<table border="1">
+					
+						<tr>
+							<td>Código</td>
+							<td>Nome</td>
+							<td>Idade</td>
+							<td>Altura</td>
+						</tr>
+						
+						<?php while($dado = $con->fetch_array()){ ?>
+						<tr>
+						<td><?php echo $dado["id"]; ?></td>
+						<td><?php echo $dado["nome"]; ?></td>
+						<td><?php echo $dado["idade"]; ?></td>
+						<td><?php echo $dado["altura"]; ?></td>
+						</tr>
+						<?php } ?>
+					</table>
+				</div>
+			</div>	
 		</div>
-		
-		<table border="1">
-			
-			<tr>
-				<td>Código</td>
-				<td>Nome</td>
-				<td>Idade</td>
-				<td>Altura</td>
-			</tr>
-			
-			<?php while($dado = $con->fetch_array()){ ?>
-			<tr>
-			<td><?php echo $dado["id"]; ?></td>
-			<td><?php echo $dado["nome"]; ?></td>
-			<td><?php echo $dado["idade"]; ?></td>
-			<td><?php echo $dado["altura"]; ?></td>
-			</tr>
-			<?php } ?>
-		</table>
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	</body>
 </html>
